78f46ad5772e64b30c9bac9609e8f3cfd9084371
mhk8691 <mohammadhosseinkaboli86@gmail.com>
2024-04-26 20:12:04 +0330

    SignUp

--- backend/weblog/User/urls.py
+++ backend/weblog/User/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.say_hello),
+    path("", views.home,name= 'home'),
+    path("signup/", views.signup,name= 'signup'),
 ]
--- backend/weblog/User/views.py
+++ backend/weblog/User/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from .forms import UserRegisterForm
+from django.contrib import messages
 
 
-def say_hello(request):
-    return HttpResponse("Hello World!")
+def signup(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(cd["username"], cd["email"], cd["password"])
+            messages.success(request, "successfuly", "success")
+            return redirect("home")
+    else:
+        form = UserRegisterForm()
+
+    return render(request, "signup.html", {"form": form})
+
+
+def home(request):
+    return render(request, "home.html")
