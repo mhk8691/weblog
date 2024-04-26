5df20ff40d06cb8ce2607682918e67067065c369
mhk8691 <mohammadhosseinkaboli86@gmail.com>
2024-04-26 22:36:12 +0330

    login and home

--- backend/weblog/User/forms.py
+++ backend/weblog/User/forms.py
@@ -4,3 +4,7 @@ class UserRegisterForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput())
     email = forms.EmailField()
+    
+class UserSigninForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput())
--- backend/weblog/User/views.py
+++ backend/weblog/User/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserSigninForm
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
 
 def signup(request):
@@ -10,7 +11,7 @@ def signup(request):
         if form.is_valid():
             cd = form.cleaned_data
             User.objects.create_user(cd["username"], cd["email"], cd["password"])
-            messages.success(request, "successfuly", "success")
+            messages.success(request, "Your registration was successful", "success")
             return redirect("home")
     else:
         form = UserRegisterForm()
@@ -20,3 +21,25 @@ def signup(request):
 
 def home(request):
     return render(request, "home.html")
+
+
+def signin(request):
+    if request.method == "POST":
+        form = UserSigninForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(
+                request, username=cd["username"], password=cd["password"]
+            )
+            if user is not None:
+                login(request, user)
+                messages.success(request, "Your login was successful", "success")
+                return redirect("home")
+            else:
+                messages.error(
+                    request, "The username or password is incorrect", "danger"
+                )
+
+    else:
+        form = UserSigninForm()
+    return render(request, "login.html", {"form": form})
